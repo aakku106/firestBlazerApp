632ba91e57db84d2626c1859684f0c81a3f36706
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-12-11 22:47:41 +0545

    add new pages for Todo, Array, and Tictak; update Counter component with conditional rendering

--- first/obj/Debug/net8.0/first.AssemblyInfo.cs
+++ first/obj/Debug/net8.0/first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be82fc44fe033df11ddd2795fad15217b082f938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad602a6890bb263bb82f3b3e384d57ca4bda75a")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
