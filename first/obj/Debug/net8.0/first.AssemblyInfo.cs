e3f64b3974127989dbc3f1278f02119f47a7229f
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2024-12-19 10:12:45 +0545

    add Form page and update NavMenu for navigation; refactor Home page with new styles and functionality

--- first/obj/Debug/net8.0/first.AssemblyInfo.cs
+++ first/obj/Debug/net8.0/first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11273f9a9b2e5cec5c0d33addbf8dc119ac03f98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1bb464f29a63aa1e5aeda2a7b19bc0d06c8069")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
